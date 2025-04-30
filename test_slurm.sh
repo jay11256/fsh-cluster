f65b35ba7ce6eeaad7432124f38b3e10adc95e85
Jason Liu <jliu1230@nexusvulcan01.umiacs.umd.edu>
2025-04-30 12:38:25 -0400

    Fix slurm params so sbatch works

--- test_slurm.sh
+++ test_slurm.sh
@@ -4,11 +4,11 @@
 #BDS 4/28/25
 
 #SBATCH --job-name=Test-script
-#SBATCH -c 16
-#SBATCH --mem=128G
+#SBATCH -c 4
+#SBATCH --mem=20G
 #SBATCH --qos=high
-#SBATCH --partition=vulcan-dpart
-#SBATCH --account=vulcan
+#SBATCH --partition=tron
+#SBATCH --account=nexus
 #SBATCH --time=540:00
 #SBATCH --mail-type=BEGIN,END,TIME_LIMIT
 #SBATCH --mail-user=jliu1230@terpmail.umd.edu
