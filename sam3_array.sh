c05d7b3e6bf09767e4cd1faddcf5bf5e0b0bed8b
Bhargav Srinivasan <bhargavdsrinivasan@gmail.com>
2026-03-06 22:32:00 -0500

    better paths

--- sam3_array.sh
+++ sam3_array.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
-#SBATCH --job-name=sam3_array
+#SBATCH --job-name=sam3pkl1_array
 #SBATCH --gres=gpu:rtxa4000:1
 #SBATCH --qos=scavenger
 #SBATCH --account=scavenger
 #SBATCH --partition=scavenger
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH --array=0-99
 #SBATCH --mail-type=BEGIN,END,TIME_LIMIT
+#SBATCH --output=/fs/vulcan-projects/fsh_track/bhargav/logs/sam3pkl2_%j.txt
+#SBATCH --error=/fs/vulcan-projects/fsh_track/bhargav/logs/sam3pkl2%_j.txt
 
-#SBATCH --output=$CLUSTER_LOGS
-#SBATCH --error=$CLUSTER_LOGS
-#SBATCH --mail-user=$CLUSTER_MAIL
-VIDEO_DIR=$SAM3_INPUT
-SCRIPT=$SAM3_SCRIPT
+# --mail-user=jliu1230@terpmail.umd.edu
+
+module load cuda
+module load ffmpeg
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate sam3
+
+VIDEO_DIR=/fs/vulcan-projects/fsh_track/processed_data/dataset3
+SCRIPT=/fs/vulcan-projects/fsh_track/jason/fsh-cluster/run_sam3.py 
+OUT_DIR=/fs/vulcan-projects/fsh_track/bhargav/data/sam3pkl2
 # ------------------------
 
 # Total number of splits = number of array jobs
 TOTAL_SPLITS=$SLURM_ARRAY_TASK_COUNT
 
-module load cuda
-module load ffmpeg
-source ~/.bashrc
-conda activate sam3   # or your env
+
+# echo $CLUSTER_MAIL
+# echo $CLUSTER_LOGS
 
 # Build sorted list of all videos
-VIDEOS=($(ls ${VIDEO_DIR}/*.mp4 | sort))
+# VIDEOS=($(ls ${VIDEO_DIR}/*.mp4 | sort))
+# NUM_VIDEOS=${#VIDEOS[@]}
+# Build sorted list of all videos (space-safe)
+mapfile -d '' VIDEOS < <(
+    find "$VIDEO_DIR" -maxdepth 1 -type f -name "*.mp4" -print0 | sort -z
+)
 NUM_VIDEOS=${#VIDEOS[@]}
 
 # Determine which subset of videos this task should process
@@ -44,11 +56,11 @@ echo
 for (( i=START_INDEX; i<END_INDEX; i++ )); do
     VIDEO=${VIDEOS[$i]}
     echo "Processing video [$i]: $VIDEO"
-    python "$SCRIPT" "$VIDEO"
+    python "$SCRIPT" --output_dir "$OUT_DIR" "$VIDEO"
     echo "Finished video [$i]: $VIDEO at $(date)"
     echo
 done
 
 echo "========== TASK $SLURM_ARRAY_TASK_ID COMPLETE =========="
 echo "End time: $(date)"
-echo "=============================================="
+# echo "=============================================="
